82aa06856afb8997b9c2902536fb7f130caf3ad4
james-o <jaydhales@gmail.com>
2023-10-08 16:51:36 +0100

    installed open zeppellin

--- src/Staker.sol
+++ src/Staker.sol
@@ -1,8 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import {IERC20} from "";
+
 contract Staker {
-    uint256 public number;
+    
 
     function setNumber(uint256 newNumber) public {
         number = newNumber;
